058ad85aeb28f980069e2a9d0d61dfd6ad538d1c
Gautam Rajput <gautamrajput1216@gmail.com>
2025-11-30 23:08:09 +0530

    Convert into Native App

---
+++ android/app/src/main/java/com/gautam/CRM/MainActivity.java
@@ -0,0 +1,5 @@
+package com.gautam.CRM;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gautam.CRM',
+  appName: 'CRM',
+  webDir: 'dist'
+};
+
+export default config;
